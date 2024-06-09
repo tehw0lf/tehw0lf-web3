bf7f36030f5169fd99487da9f77726328b75912d
tehw0lf <tehwolf@protonmail.com>
2024-06-09 20:51:17 +0200

    Add form boilerplate

--- apps/xyz/src/app/xyz/xyz.component.ts
+++ apps/xyz/src/app/xyz/xyz.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NeoApiService } from '@tehw0lf/neo-api';
+import { Observable } from 'rxjs';
+
+import { XTokenComponent } from './x-token/x-token.component';
+import { YTimeComponent } from './y-time/y-time.component';
+import { ZPriceComponent } from './z-price/z-price.component';
 
 @Component({
   selector: 'app-xyz',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    XTokenComponent,
+    YTimeComponent,
+    ZPriceComponent,
+  ],
+  providers: [NeoApiService],
   templateUrl: './xyz.component.html',
 })
-export class XyzComponent {}
+export class XyzComponent {
+  result$: Observable<number> | null = null;
+  constructor(private neoApiService: NeoApiService) {}
+
+  onSubmit(form: NgForm): void {
+    if (form.form.valid) {
+      this.neoApiService.getFlamingoLivedataPricesFromtimestamp(
+        form.value.timestamp
+      );
+    }
+  }
+}
